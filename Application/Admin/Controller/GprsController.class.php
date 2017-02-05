c750f86efd9778f08ecd221a3a755b9c0a5acec9
apoptusis <364567043@qq.com>
2017-02-06 04:32:36 +0800

    123

--- Application/Admin/Controller/GprsController.class.php
+++ Application/Admin/Controller/GprsController.class.php
@@ -21,7 +21,7 @@ class GprsController extends CommonController {
             $data['force']=round($data['weight']*$data['amplitude']*9.8,2);
             // 10%的概率报警
             $warning = mt_rand(0,100);
-            if($warning >= 90){
+            if($warning >= 97){
                 $isWarning = 1;
             }else {
                 $isWarning = 0;
@@ -44,6 +44,8 @@ class GprsController extends CommonController {
         randomDataInSQL(1003,13577589542,112.85,27.90);
         //1004号塔机添加数据
         randomDataInSQL(1004,18777665599,112.80,28.00);
+        //1005号塔机添加数据
+        randomDataInSQL(1005,1897654287,112.91,27.91);
 
         // 10s刷新一次页面
         echo ("<script type=\"text/javascript\">");
