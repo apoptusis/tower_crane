47c4a6e717154c891a922bb2f1c15f2e99b39406
apoptusis <364567043@qq.com>
2017-02-08 04:16:37 +0800

    邮箱验证码后端校验

--- Application/Home/Controller/TowerCraneController.class.php
+++ Application/Home/Controller/TowerCraneController.class.php
@@ -1,6 +1,8 @@
 <?php
 namespace Home\Controller;
 use Think\Controller;
+import('Vendor.PHPMailer.PHPMailer');
+import('Vendor.PHPMailer.Smtp');
 class towerCraneController extends Controller {
     public function login(){
         if(IS_POST){
@@ -22,9 +24,13 @@ class towerCraneController extends Controller {
             }else{
                 // 发给客户端的登录token
                 $token = getMd5($username+time());
+                $data = array(
+                    'token' => $token,
+                    'username' => $username,
+                );
                 cookie('username',$username);
                 cookie('tokenId',$token);
-                return show(1,'登录成功',$token);
+                return show(1,'登录成功',$data);
             }
         }
     }
@@ -97,12 +103,10 @@ class towerCraneController extends Controller {
             }
             $warningTime = array_count_values($warning)[1];
             $warn = array('warningTime' => $warningTime);
-
             // 查询塔机基本信息，如额定力矩
             $modelData = D('Cranereg')->where('sim_num='.$sim_num)->select();
             $model = $modelData[0]['model'];
             $baseData = D('Baseinfo')->where("model='".$model."'")->select();
-
             // 计算在线时间
             for($i = 0; $i < count($updateTime); $i++){
                 $diff = $updateTime[$i-1] - $updateTime[$i];
@@ -118,10 +122,8 @@ class towerCraneController extends Controller {
                 'endTime' => $endTime,
                 'duration' => $duration,
             );
-
             // 拼接数组
             $data = array_merge($realData[0],$baseData[0],$warn,$workTime);
-
             // 返回数据
             if($realData && $modelData && $baseData){
                 return show(1, '查询成功', $data);
@@ -135,7 +137,7 @@ class towerCraneController extends Controller {
     public function historyData(){
         if(IS_POST){
             $sim_num = I('post.sim_num');
-            $res = D('Realinfo')->where('sim_num='.$sim_num)->order('update_time')->limit(100)->select();
+            $res = D('Realinfo')->where('sim_num='.$sim_num)->order('update_time desc')->limit(100)->select();
             foreach ($res as $k => $v) {
                 $data[$k]['update_time'] = $v['update_time'];
                 $data[$k]['weight'] = $v['weight'];
@@ -153,6 +155,84 @@ class towerCraneController extends Controller {
         }
         $this->display();
     }
+
+
+    public function findUserInfo(){
+        if(IS_POST) {
+            $username = I('post.username');
+            $res = D('User')->where('username="' . $username . '"')->select();
+            if ($res) {
+                return show(1, '查询成功', $res[0]);
+            } else {
+                return show(0, '数据查询失败');
+            }
+        }
+    }
+
+    public function sendEmail(){
+        if(IS_POST) {
+            // 获取用户名和邮箱
+            $username = I('post.username');
+            $userEmail = I('post.email');
+            $username = 'admin';
+            $userEmail = '364567043@qq.com';
+            // 生成验证码
+            $identifyNum = mt_rand(100000, 999999);
+            $data = array(
+                'identifyNum' => $identifyNum,
+            );
+            // 存入cookie
+            $identifyString = $username.$identifyNum;
+            cookie('identifyString',$identifyString,30);
+            // 发送验证码邮件
+            try{
+                $mail = new \PHPMailer(true);
+                $mail->IsSMTP();
+                $mail->CharSet = 'UTF-8';
+                $mail->SMTPAuth = true;
+                $mail->Port = 25;
+                $mail->Host = "smtp.163.com";
+                // 发送邮箱
+                $mail->Username = "18673244876@163.com";
+                // 这里是STMP服务的密码
+                $mail->Password = "Letmegofuck123";
+                // 回复地址
+                $mail->AddReplyTo("18673244876@163.com", "Hwasin");
+                // 寄信人
+                $mail->From = "18673244876@163.com";
+                $mail->FromName = "塔机监控系统APP";
+                $mail->AddAddress($userEmail);
+                // 邮件内容
+                $mail->Subject = "你更改帐号信息的验证码是:" . $identifyNum;
+                $mail->Body = "
+                    <h2>" . $username . "，你好，<h2>
+                    <p>此次帐号信息变更需要的验证码如下，请在 30 分钟内输入验证码进行下一步操作。</p>
+                    <p style='font-size:32px;font-weight:700;color:#ff545b;'>" . $identifyNum . "</p>
+                    <p>如果非你本人操作，你的帐号可能存在安全风险，请立即修改密码!</p>
+                    ";
+                $mail->WordWrap = 80; // 设置每行字符串的长度
+                $mail->IsHTML(true);
+                $mail->Send();
+                return show(1, '验证码发送成功,请注意查收', $data);
+            } catch (phpmailerException $e) {
+                return show(0, '验证码发送失败,请联系客服');
+            }
+        }
+    }
+
+    public function checkIdentifyNum(){
+        if(IS_POST){
+            $username = I('post.username');
+            $identifyNum = I('post.identifyNum');
+            $userIdentifyString = $username.$identifyNum;
+            if(cookie('identifyString') == $userIdentifyString){
+                return show(1, '验证成功');
+            }else{
+                return show(0, '验证码错误');
+            }
+        }
+    }
+
 
     public function showPassword(){
         echo getMd5('admin1');
