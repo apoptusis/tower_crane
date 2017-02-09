f231e02b18762a7091db5c09d9456cb411c5d9e6
apoptusis <364567043@qq.com>
2017-02-09 09:35:08 +0800

    添加手机号、email格式的自动验证

--- Application/Common/Model/UserModel.class.php
+++ Application/Common/Model/UserModel.class.php
@@ -8,5 +8,10 @@ class UserModel extends Model{
         array('newPwdConfirm','require','确认密码不能为空！'),
         array('newPwdConfirm','6,16','确认密码范围在6-16位！',0,'length',3),
         array('newPwd','newPwdConfirm','两次密码不一致！',0,'confirm'),
+        array('newPhone','require','手机号码不能为空！'),
+        array('newPhone','/^1[3|4|5|8][0-9]\d{8}$/','手机号码格式错误！','0','regex',1),
+        array('newPhone','newPhoneConfirm','两次密码输入的手机号码不一致！',0,'confirm'),
+        array('email','require','邮箱不能为空！'),
+        array('email','email','请输入正确的邮箱地址！'),
     );
 }
