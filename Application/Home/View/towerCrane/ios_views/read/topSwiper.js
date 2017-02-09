265d8d005c6764d53260cc12e9ab4324b3a6e5f5
apoptusis <364567043@qq.com>
2017-02-10 04:02:41 +0800

    建立read模块

---
+++ Application/Home/View/towerCrane/ios_views/read/topSwiper.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    TouchableOpacity,
+} from 'react-native';
+import Util from '../common/util';
+
+export default class TopScrollBar extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <View>
+                <Text>123</Text>
+            </View>
+        );
+    }
+}
+
+var styles = StyleSheet.create({});
+
+module.exports = topSwiper;
