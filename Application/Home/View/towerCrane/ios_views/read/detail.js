38288926cb4f871d876b4315d76bd98bbe7d0dc5
apoptusis <364567043@qq.com>
2017-02-10 05:32:00 +0800

    显示WebView详情组件

---
+++ Application/Home/View/towerCrane/ios_views/read/detail.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+    Image,
+    TouchableOpacity,
+} from 'react-native';
+import WView from '../common/WView';
+import NavigationBar from '../common/navBar'
+
+export default class Detail extends Component {
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return (
+            <View style={styles.container}>
+                <NavigationBar
+                    title={'实时数据'}
+                    leftText={'返回'}
+                    leftAction={ this._backToFront.bind(this) }
+                />
+                <WView
+                    url={this.props.url}
+                />
+            </View>
+        );
+    }
+    _backToFront (){
+        const { navigator } = this.props;
+        if(navigator) {
+            navigator.pop();
+        }
+    }
+}
+
+var styles = StyleSheet.create({
+    container : {
+        flex: 1
+    },
+});
+
+module.exports = Detail;
