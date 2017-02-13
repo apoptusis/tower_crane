40c3ea04328f51f82c6d83d4e9859c211b85f69a
apoptusis <364567043@qq.com>
2017-02-13 20:44:38 +0800

    添加用户引导

--- Application/Home/View/towerCrane/ios_views/setting/help.js
+++ Application/Home/View/towerCrane/ios_views/setting/help.js
@@ -6,20 +6,68 @@ import {
     Image,
 } from 'react-native';
 import NavigationBar from '../common/navBar';
+import Util from '../common/util';
+import Swiper from 'react-native-swiper';
 
+var step1 = '';
 
 export default class help extends Component {
     render() {
         return (
-            <View>
+            <View style={styles.container}>
                 <NavigationBar
                     title={'帮助中心'}
                     leftText={'设置'}
                     leftAction={ this._backToFront.bind(this) }
                 />
-                <Text>
-                    帮助
-                </Text>
+                <Swiper style={styles.wrapper}
+                        dot={<View style={{backgroundColor: 'rgba(0,0,0,0.1)', width: 13, height: 13, borderRadius: 7, marginLeft: 7, marginRight: 7}} />}
+                        activeDot={<View style={{backgroundColor: '#008edb', width: 13, height: 13, borderRadius: 7, marginLeft: 7, marginRight: 7}} />}
+                        paginationStyle={{
+                            bottom: 70
+                        }}
+                        loop={false}>
+                    <Image
+                        resizeMode='cover'
+                        style={styles.image}
+                        source={{url:'http://localhost:8888/tower_crane/Application/Home/View/towerCrane/images/step1.png'}}/>
+                    <Image
+                        resizeMode='cover'
+                        style={styles.image}
+                        source={{url:'http://localhost:8888/tower_crane/Application/Home/View/towerCrane/images/step2.png'}}/>
+                    <Image
+                        resizeMode='cover'
+                        style={styles.image}
+                        source={{url:'http://localhost:8888/tower_crane/Application/Home/View/towerCrane/images/step3.png'}}/>
+                    <Image
+                        resizeMode='cover'
+                        style={styles.image}
+                        source={{url:'http://localhost:8888/tower_crane/Application/Home/View/towerCrane/images/step4.png'}}/>
+                    <Image
+                        resizeMode='cover'
+                        style={styles.image}
+                        source={{url:'http://localhost:8888/tower_crane/Application/Home/View/towerCrane/images/step5.png'}}/>
+                    <Image
+                        resizeMode='cover'
+                        style={styles.image}
+                        source={{url:'http://localhost:8888/tower_crane/Application/Home/View/towerCrane/images/step6.png'}}/>
+                    <Image
+                        resizeMode='cover'
+                        style={styles.image}
+                        source={{url:'http://localhost:8888/tower_crane/Application/Home/View/towerCrane/images/step7.png'}}/>
+                    <Image
+                        resizeMode='cover'
+                        style={styles.image}
+                        source={{url:'http://localhost:8888/tower_crane/Application/Home/View/towerCrane/images/step8.png'}}/>
+                    <Image
+                        resizeMode='cover'
+                        style={styles.image}
+                        source={{url:'http://localhost:8888/tower_crane/Application/Home/View/towerCrane/images/step9.png'}}/>
+                    <Image
+                        resizeMode='cover'
+                        style={styles.image}
+                        source={{url:'http://localhost:8888/tower_crane/Application/Home/View/towerCrane/images/step10.png'}}/>
+                </Swiper>
             </View>
         );
     }
@@ -32,7 +80,18 @@ export default class help extends Component {
 }
 
 const styles = StyleSheet.create({
-
+    container: {
+        height: Util.size.height,
+        width: Util.size.width,
+        backgroundColor: '#fff'
+    },
+    wrapper: {
+    },
+    image: {
+        width: Util.size.width,
+        height: Util.size.height-20,
+        marginTop: -40,
+    }
 });
 
 module.exports = help;
